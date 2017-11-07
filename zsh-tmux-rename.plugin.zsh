aeb4588b8525464b92bf414a0e16b137090644b5
Seong Yong-ju <sei40kr@gmail.com>
2017-11-07 23:16:03 +0900

    Renamed hook function name

--- zsh-tmux-rename.plugin.zsh
+++ zsh-tmux-rename.plugin.zsh
@@ -5,9 +5,9 @@
 
 autoload -Uz add-zsh-hook
 
-add-zsh-hook precmd precmd_tmux_rename
+add-zsh-hook precmd __precmd_tmux_rename
 
-precmd_tmux_rename() {
+__precmd_tmux_rename() {
   local dir="$(git rev-parse --show-toplevel 2>/dev/null)"
   if [ -n "$dir" ]
   then
