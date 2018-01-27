07667f9ddb32bda92f4a72acf01eb2c9d34f48e4
Seong Yong-ju <sei40kr@gmail.com>
2018-01-28 00:15:37 +0900

    Update source code and README

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # zsh-tmux-rename
 
-A zsh plugin to rename tmux window automatically.
+A **ZSH** plugin to rename tmux window automatically.
+
+<!-- vim-markdown-toc GFM -->
+
+- [How it works](#how-it-works)
+- [Installation](#installation)
+  - [Example using zplug](#example-using-zplug)
+
+<!-- vim-markdown-toc -->
 
 ## How it works
 
@@ -10,9 +18,15 @@ A zsh plugin to rename tmux window automatically.
 
 ### Example using zplug
 
-Add the following command to your `.zshrc`.
+1. I recommend you to use [zplugin](https://github.com/zdharma/zplugin) to load the plugin lazily.
 
-```zsh
-zplug 'sei40kr/zsh-tmux-rename'
-```
+    ```zsh
+    zplugin ice wait'1'
+    zplugin light 'sei40kr/zsh-tmux-rename'
+    ```
 
+1. If you're going to use [zplug](https://github.com/zplug/zplug).
+
+    ```zsh
+    zplug 'sei40kr/zsh-tmux-rename'
+    ```
--- zsh-tmux-rename.plugin.zsh
+++ zsh-tmux-rename.plugin.zsh
@@ -3,25 +3,25 @@
 # zsh-tmux-rename.plugin.zsh
 # author: Seong Yong-ju ( @sei40kr )
 
-autoload -Uz add-zsh-hook
+-precmd-zsh-tmux-rename() {
+  [[ -z "$TMUX" ]] && return 0
 
-add-zsh-hook precmd __precmd_tmux_rename
+  local repo_root="$(git rev-parse --show-toplevel 2>/dev/null)"
 
-__precmd_tmux_rename() {
-  if [[ -n "$TMUX" ]]
+  if [[ -n "$repo_root" ]]
   then
-    (
-      {
-        local git_rootdir="`git rev-parse --show-toplevel 2>/dev/null`"
-
-        if [[ -n "$git_rootdir" ]]
-        then
-          tmux rename-window "`basename \"$git_rootdir\"`"
-        else
-          tmux set-window-option automatic-rename
-        fi
-      } &;
-    ) >/dev/null
+    tmux rename-window "${repo_root##*/}"
+  else
+    tmux rename-window "${PWD##*/}"
   fi
 }
 
+main() {
+  autoload -Uz add-zsh-hook
+  add-zsh-hook precmd -precmd-zsh-tmux-rename
+
+  tmux set-window-option allow-rename on
+  tmux set-window-option automatic-rename off
+}
+
+main
